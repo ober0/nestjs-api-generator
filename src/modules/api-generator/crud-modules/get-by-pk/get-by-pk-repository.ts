4a2a65462fe6aae2f462e0ce709a868c14b2d848
Ruslan <onishruslan@yandex.ru>
2025-08-13 00:44:36 +0300

    обработал ошибку pk

--- src/modules/api-generator/crud-modules/get-by-pk/get-by-pk-repository.ts
+++ src/modules/api-generator/crud-modules/get-by-pk/get-by-pk-repository.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common'
 import { GeneratorMainDto } from '../../dto/generator/main.dto'
 import { OrmEnum } from '../../enums/orm.enum'
 
@@ -8,17 +8,30 @@ export function createCrudGetByPkRepository(data: GeneratorMainDto, dbServiceTok
         constructor(@Inject(dbServiceToken) readonly dbService: any) {}
 
         async getByPk(pk: any): Promise<InstanceType<typeof data.methods.getByPk.responseType>> {
-            let result
-            switch (data.db.orm) {
-                case OrmEnum.Prisma:
-                    result = await this.dbService[data.db.model].findUnique({
-                        where: { [data.db.pk]: pk }
-                    })
-                    break
-                default:
-                    throw new Error(`ORM ${data.db.orm} not supported in getByPk method`)
+            try {
+                let result
+                switch (data.db.orm) {
+                    case OrmEnum.Prisma:
+                        result = await this.dbService[data.db.model].findUnique({
+                            where: { [data.db.pk]: pk }
+                        })
+                        break
+                    default:
+                        throw new Error(`ORM ${data.db.orm} not supported in getByPk method`)
+                }
+                return result
+            } catch (error) {
+                switch (data.db.orm) {
+                    case OrmEnum.Prisma:
+                        if (error.name === 'PrismaClientValidationError') {
+                            throw new InternalServerErrorException(`Указано некорректное значение pk в схеме для таблицы ${data.db.model}`)
+                        }
+                        break
+                    default:
+                        throw new Error(`ORM ${data.db.orm} not supported in getByPk method`)
+                }
+                throw error
             }
-            return result
         }
     }
     return CrudGetByPkRepository
